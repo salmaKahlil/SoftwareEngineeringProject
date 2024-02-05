5a3c7f5989a29ad64a344e69430b88e839494ad1
salmaKahlil <157662822+salmaKahlil@users.noreply.github.com>
2024-02-04 23:55:24 -0500

    Create Engine Coordinator API

---
+++ Engine Coodinator API.java
@@ -0,0 +1,19 @@
+//Prototype the Use Case
+    public void initiateComputation(transferRespose dataTransfered) {
+        // Create a compuation request 
+        ComputationRequest computationRequest = new ComputationRequest(dataTransfered.getData());
+
+        // Perform the actual computation
+        ComputationResponse computationResponse = computeEngine.performComputation(computationRequest);
+    }
+    //Wrapper Interface
+    public Interface computeEngineComponents{
+        ComputationRequest computationRequest(dataLoadResponse data);
+        ComputationResponse performComputation(dataForComputation data);
+    }
+
+    
+    
+
+
+
