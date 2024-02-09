7ca0e2fb527ff073f5ca142a87ce0a1f5fbd1186
salmaKahlil <157662822+salmaKahlil@users.noreply.github.com>
2024-02-09 16:22:41 -0500

    checksUpdates

--- Data.java
+++ Data.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public interface Data{
+public interface Data {
     ArrayList<Integer> arr(IntegersFromTheUser numbers);
     ComputeMessage computationEndMessage(OutputDetails resultDest, String results);
 }
