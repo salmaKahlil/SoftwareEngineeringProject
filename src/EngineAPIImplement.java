6aeee503b8f1adb2fd28863e2c93deacd24f5e19
grimalh <157427701+grimalh@users.noreply.github.com>
2024-02-11 20:38:56 -0500

    implementations
    ?

---
+++ src/EngineAPIImplement.java
@@ -0,0 +1,23 @@
+public class EngineAPIImplement implements ComputeEngine, ComputeRequest  {
+    
+    @Override
+    public String compute(int number) {
+    
+        return null;
+    }
+
+    @Override
+    public IntegersFromTheUser getIntegerFromTheUser() {
+    return null;
+    }
+
+    @Override
+    public char getDelimeters() {
+      return 0;
+    }
+
+    @Override
+    public OutputDetails getOutputDetails() {
+       return null;
+    }
+}
