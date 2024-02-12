7c3a96cc910745bec46c5faeebaecde52517158a
grimalh <157427701+grimalh@users.noreply.github.com>
2024-02-11 20:46:51 -0500

    implementations?

--- src/UserAPIImplement.java
+++ src/UserAPIImplement.java
@@ -1,14 +1,5 @@
 public class UserAPIImplement {
 
-   
-    public IntegersFromTheUser getIntegerFromTheUser;
-    
-    public char getDelimeters;
-    
-    public OutputDetails getOutputDetails;
 
-
-      
-    
-
+  
 }
---
+++ test/SmokeTests.java
@@ -0,0 +1,12 @@
+package test;
+import org.mockito.Mockito; 
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.any;
+
+public class SmokeTests {
+   
+
+}
