dae5b26281d29e819ee052301f77bddf5c18f08a
mairanadeem97 <157428941+mairanadeem97@users.noreply.github.com>
2024-02-13 16:29:48 -0500

    Update ComputeEngineIntegrationTest.java

--- test/ComputeEngineIntegrationTest.java
+++ test/ComputeEngineIntegrationTest.java
@@ -1,5 +1,32 @@
 package test;
 
-public class ComputeEngineIntegrationTest {
-    
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ComputeEngineIntegrationTest  {
+    {
+        @Test
+        public void TestComputeEngineIntegration()
+        {
+           
+            //initializing the data
+            int [] inputConfig={1,10,25};
+            //not specifying delimter
+            String delimeter=null;
+          //creating instances of components
+       
+       ComputeEngineImp computeengine = new ComputeEngineImp();
+       DataImp     dataimp= new DataImp();
+       computeengine.compute(null);
+       dataimp.computationEndMessage(null, delimeter);
+    IntegersFromTheUser numbers;
+     dataimp.dataIn(numbers);
+     String outputString=computeengine.compute(null);
+      assertNotNull(null);
+
+        }
+
+        
 }
