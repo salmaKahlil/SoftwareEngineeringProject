62a37e8b1a2b943feade7883940662e9e6750fcc
salmaKahlil <157662822+salmaKahlil@users.noreply.github.com>
2024-02-13 14:53:42 -0500

    Three API tested and Integration infrastructure done

--- src/ComputeEngine.java
+++ src/ComputeEngine.java
@@ -1,7 +1,4 @@
+import java.util.ArrayList;
 public interface ComputeEngine {
-<<<<<<< Updated upstream
-    String compute(int number);
-=======
     public String compute(ArrayList<Integer> number);
->>>>>>> Stashed changes
 }
