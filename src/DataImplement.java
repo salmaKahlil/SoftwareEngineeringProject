4851b0461ee89e72150dfc970218f56bff300f1b
grimalh <157427701+grimalh@users.noreply.github.com>
2024-02-13 14:16:06 -0500

    Create DataImplement.java

---
+++ src/DataImplement.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class DataImplement implements Data {
+
+    @Override
+    public ArrayList<Integer> arr(IntegersFromTheUser numbers) {
+    return null; 
+    }
+
+    @Override
+    public ComputeMessage computationEndMessage(OutputDetails resultDest, String results) {
+
+        return null; 
+    }
+
+    
+    
+   
+}
