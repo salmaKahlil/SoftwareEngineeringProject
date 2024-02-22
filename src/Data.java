2c8262967db079881bf8731a00d2c0f3d0ac0bb4
mairanadeem97 <157428941+mairanadeem97@users.noreply.github.com>
2024-02-21 22:17:53 -0500

    Retesting (#44)
    Retesting APIS, after some modifications.

--- src/Data.java
+++ src/Data.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public interface Data {
+    /*HELLO */
     ArrayList<Integer> arr(IntegersFromTheUser numbers);
     ComputeMessage computationEndMessage(OutputDetails resultDest, String results);
 }
