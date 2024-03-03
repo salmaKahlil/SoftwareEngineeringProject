d46ade5b584c2623ac3acc47521b24aa98bd8fd4
salmaKahlil <157662822+salmaKahlil@users.noreply.github.com>
2024-03-02 23:58:14 -0500

    Part 2  (#49)
    * Program Implementation and Testin
    
    * Part 2 done
    
    * changing the variable name to pass the checks

---
+++ test/software/project/TestMultiUsers.java
@@ -0,0 +1,82 @@
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class TestMultiUsers {
+
+	private SystemCoordinator coordinator;
+	
+	@Before
+	public void initializeComputeEngine() {
+		Data dataTest = Mockito.mock(Data.class);
+		ComputeEngine computeEngineTest = Mockito.mock(ComputeEngine.class);
+		coordinator = new SystemCoordinatorImp(dataTest, computeEngineTest);
+	}
+
+	@Test
+	public void compareMultiAndSingleThreaded() throws Exception {
+		int numThreads = 4;
+		List<TestUser> testUsers = new ArrayList<>();
+		for (int i = 0; i < numThreads; i++) {
+			testUsers.add(new TestUser(coordinator));
+		}
+		
+		// Run single threaded
+		String singleThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.singleThreadOut.tmp";
+		for (int i = 0; i < numThreads; i++) {
+			File singleThreadedOut = new File(singleThreadFilePrefix + i);
+			singleThreadedOut.createNewFile(); // Create the file
+			singleThreadedOut.deleteOnExit();
+			testUsers.get(i).run(singleThreadedOut.getCanonicalPath());
+			
+		}
+		
+		// Run multi threaded
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+		List<Future<?>> results = new ArrayList<>();
+		String multiThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.multiThreadOut.tmp";
+		for (int i = 0; i < numThreads; i++) {
+			File multiThreadedOut = new File(multiThreadFilePrefix + i);
+			multiThreadedOut.createNewFile(); // Create the file
+			multiThreadedOut.deleteOnExit();
+			String multiThreadOutputPath = multiThreadedOut.getCanonicalPath();
+			TestUser testUser = testUsers.get(i);
+			results.add(threadPool.submit(() -> testUser.run(multiThreadOutputPath)));
+		}
+		
+		results.forEach(future -> {
+			try {
+				future.get();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
+		
+		
+		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, numThreads);
+		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, numThreads);
+		Assert.assertEquals(singleThreaded, multiThreaded);
+	}
+
+	private List<String> loadAllOutput(String prefix, int numThreads) throws IOException {
+		List<String> result = new ArrayList<>();
+		for (int i = 0; i < numThreads; i++) {
+			File multiThreadedOut = 
+					new File(prefix + i);
+			result.addAll(Files.readAllLines(multiThreadedOut.toPath()));
+		}
+		return result;
+	}
+}
---
+++ test/software/project/TestUser.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+
+public class TestUser {
+	
+	private final SystemCoordinator coordinator;
+
+	public TestUser(SystemCoordinator coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		char delimiter = ';';
+		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		
+	}
+
+}
