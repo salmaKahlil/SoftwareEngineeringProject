7b00127e3eefb9b190ffccc2423d4315a6ebfd9d
grimalh <157427701+grimalh@users.noreply.github.com>
2024-04-29 16:23:01 -0400

    Create ComputeEngineBenchmarkTest.java

---
+++ test/software/project/ComputeEngineBenchmarkTest.java
@@ -0,0 +1,11 @@
+package software.project;
+
+public class ComputeEngineBenchmarkTest {
+
+    private ComputeEngine computeEngine = new ComputeEngine();
+
+
+public void computeSpeed(){
+    computeEngine.compute(2,3);
+}
+}
