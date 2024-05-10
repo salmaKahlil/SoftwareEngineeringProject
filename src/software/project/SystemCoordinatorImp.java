c3b5fb4d00b590300352fa0ef42707f0f1e61d75
salmaKahlil <157662822+salmaKahlil@users.noreply.github.com>
2024-05-09 23:31:46 -0400

    To pass the checks p2

--- src/software/project/SystemCoordinatorImp.java
+++ src/software/project/SystemCoordinatorImp.java
@@ -1,10 +1,12 @@
 package software.project;
 
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.*;
+
 
 
 public class SystemCoordinatorImp implements SystemCoordinator {
